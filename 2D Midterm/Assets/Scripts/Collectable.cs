2f9945533982b9eeea8ebf638dcadccf8d49bb90
Ming Wu <ming.wu@loop.colum.edu>
2019-10-22 12:27:23 -0500

    2D Collectables, animation, and static variables
    2D Collectables, animation, and static variables

---
+++ 2D Midterm/Assets/Scripts/Collectable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private static int coinCount;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            coinCount++;
+            Debug.Log("Coin count: " + coinCount);
+            audioSource.Play();
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            Destroy(gameObject, audioSource.clip.length);
+        }
+    }
+}
