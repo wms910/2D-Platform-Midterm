533d72c57ab6a8298e3363f4bd7fe8bce16dbe51
Ming Wu <ming.wu@loop.colum.edu>
2019-10-22 11:52:59 -0500

    Doors, Loading New Scenes
    Doors, Loading New Scenes

---
+++ 2D Midterm/Assets/Scripts/Door.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    private bool isPllayerInTrigger;
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        if (Input.GetButtonDown("Activate"))
+    //        {
+    //            Debug.Log("Player activated door!");
+    //        }
+            
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPllayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPllayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPllayerInTrigger)
+        {
+            Debug.Log("Player activated door!");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
