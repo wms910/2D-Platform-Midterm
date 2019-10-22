dd5247585e8fda906309a20420a090e400da0efe
Ming Wu <ming.wu@loop.colum.edu>
2019-10-21 23:10:26 -0500

    2D Input and Basic Character Movement
    2D Input and Basic Character Movement

---
+++ 2D Midterm/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    private float horzontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horzontalInput = Input.GetAxis("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.AddForce(Vector2.right * horzontalInput * speed);
+
+    }
+}
