f3bb66257c7807fa2201f86ea2c9115507026b57
Ming Wu <ming.wu@loop.colum.edu>
2019-10-22 09:44:18 -0500

    GES 2D Side-scroller Tutorial Part 9 - Trigger-based Hazards and Reloading Scenes
    GES 2D Side-scroller Tutorial Part 9 - Trigger-based Hazards and Reloading Scenes

---
+++ 2D Midterm/Assets/Scripts/Hazard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Something entered the hazard.");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            Debug.Log("Something other then player entered the hazard.");
+        }
+    }
+}
